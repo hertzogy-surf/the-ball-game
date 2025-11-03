223cfbee95bfbf152d5e798db7ee6af738b97d0f
Yosef <hertzogy@gmail.com>
2025-11-03 23:14:47 +0200

    Make the ball grow when clicked

--- css/js/main.js
+++ css/js/main.js
@@ -1,3 +1,12 @@
-function onBallClicked() {
-    console.log('ball clidked')
+var gBallWidth = gBallHeight = 100
+
+function onBallClicked(elDiv) {
+    
+    gBallWidth += 50
+    gBallHeight += 50
+
+    elDiv.style.width = gBallWidth + 'px'
+    elDiv.style.height = gBallHeight + 'px'
+
+   
 }
