05442f32a6dfc3aff938f0555b80f31a57a0c1c9
Yosef <hertzogy@gmail.com>
2025-11-03 23:30:33 +0200

    Resize the ball if up to 400px and add transition

--- css/js/main.js
+++ css/js/main.js
@@ -2,11 +2,17 @@ var gBallWidth = gBallHeight = 100
 
 function onBallClicked(elDiv) {
     
-    gBallWidth += 50
-    gBallHeight += 50
+    if (gBallWidth === 400) {
+        gBallWidth = 100
+        gBallHeight = 100
+    }
+    else {
+        gBallWidth += 50
+        gBallHeight += 50
+    }
 
     elDiv.style.width = gBallWidth + 'px'
     elDiv.style.height = gBallHeight + 'px'
 
-   
+
 }
